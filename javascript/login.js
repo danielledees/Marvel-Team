d6d36156b8f0dc217941c6a890aa5d1f9e5a4769
Danielle Dees <d.danielle83@gmail.com>
2018-12-22 11:59:46 -0500

    updated

--- javascript/login.js
+++ javascript/login.js
@@ -7,12 +7,18 @@ var config = {
     storageBucket: "marvelproject-40bbc.appspot.com",
     messagingSenderId: "1078044173233"
   };
+
   firebase.initializeApp(config);
 
   var database = firebase.database();
 
+
+
   // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    // Uploads employee data to the database
+    //database.ref().push(ui);
 
 
 var uiConfig = {
@@ -36,11 +42,106 @@ var uiConfig = {
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
-    // Terms of service url.
-    //tosUrl: 'login.html',
-    // Privacy policy url.
-    //privacyPolicyUrl: '<your-privacy-policy-url>'
+    
   };
 
-  // The start method will wait until the DOM is loaded.
+    // The start method will wait until the DOM is loaded.
 ui.start('#firebaseui-auth-container', uiConfig);
+
+
+
+  function renderImages(){
+  var queryURL =  "https://api.giphy.com/v1/gifs/search?q=marvel&api_key=xQFr9FVDliZZP58r6lj2oZ94REMLxL68&limit=10";
+   console.log(queryURL);
+
+   $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+       console.log(response, "this is the response");
+       //var matches = response.data;
+       // Saving the image_original_url property
+       var matches = response.data.image_original_url;
+       //runs loop thru the data repsonses and appends images to display-gifs div with rating
+
+       //for (var i = 0; i <matches.length; i++) {
+
+        // Creating and storing an image tag
+        var marvelImage = $("<img>");
+
+        // Setting the catImage src attribute to imageUrl
+        marvelImage.attr("src", matches);
+        marvelImage.attr("alt", "marvel image");
+
+        // Prepending the catImage to the images div
+        //$("#display-gifs").prepend(marvelImage);
+
+            $("#display-gifs").append(marvelImage);
+            console.log(marvelImage);
+
+    })
+    }
+
+
+
+
+
+renderImages()
+
+
+
+
+//display marvel gifs 
+
+// my API Key xQFr9FVDliZZP58r6lj2oZ94REMLxL68
+
+//var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=marvel&api_key=xQFr9FVDliZZP58r6lj2oZ94REMLxL68&limit=5");
+//xhr.done(function(data) { console.log("success got data", data); });
+
+
+    
+
+    /*// After the data from the AJAX request comes back
+    .then(function(response) {
+        console.log(response);
+
+
+        
+
+        // Saving the image_original_url property
+          var imageUrl = response.data.image_original_url;
+
+          // Creating and storing an image tag
+          var marvelImage = $("<img>");
+
+          // Setting the catImage src attribute to imageUrl
+          marvelImage.attr("src", imageUrl);
+          marvelImage.attr("alt", "marvel image");
+
+          // Prepending the catImage to the images div
+          $("#display-gifs").prepend(marvelImage);
+
+    })
+    */
+
+
+  
+
+
+
+   
+   
+
+
+
+
+
+
+    
+
+
+
+
+
