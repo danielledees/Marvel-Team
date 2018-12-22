674e87143f9e9ed034584375eded7333f41c0461
Danielle Dees <d.danielle83@gmail.com>
2018-12-21 21:06:16 -0500

    log in page

---
+++ javascript/login.js
@@ -0,0 +1,46 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCRRRYCURmR-Xbt8ks2q5iacgBVoQI0f7M",
+    authDomain: "marvelproject-40bbc.firebaseapp.com",
+    databaseURL: "https://marvelproject-40bbc.firebaseio.com",
+    projectId: "marvelproject-40bbc",
+    storageBucket: "marvelproject-40bbc.appspot.com",
+    messagingSenderId: "1078044173233"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  // Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'main.html',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+    // Terms of service url.
+    //tosUrl: 'login.html',
+    // Privacy policy url.
+    //privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+  // The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
